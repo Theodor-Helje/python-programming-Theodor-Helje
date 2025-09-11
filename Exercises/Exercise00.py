aa3ce3c52137868aa4616c2134244ddbcf7d2cb5
Theodor Helje <Theodor.Helje@iths.se>
2025-09-11 11:48:31 +0200

    finished exercises 00

--- Exercises/Exercise00.py
+++ Exercises/Exercise00.py
@@ -28,3 +28,28 @@ print (f"3: the model has an accuracy of {accuracy} or {round(accuracy * 100, 1)
 print(f"given the {FN} false negatives, this is not a good model.")
 
 #4
+AX = 4
+AY = 4
+BX = 0
+BY = 1
+k = (BY - AY) / (BX - AX)
+m = AY - (k * AX)
+print (f"4: the equation of the slope is y = {k}x + {m}")
+
+#5
+p1X = 3
+p1Y = 5
+p2X = -2
+p2Y = 4
+distance = math.sqrt((p2X - p1X) ** 2 + (p2Y - p1Y) ** 2)
+print (f"5: the distance in 2D between p1 and p2 is {round(distance, 1)} units")
+
+#6
+p1X = 2
+p1Y = 1
+p1Z = 4
+p2X = 3
+p2Y = 1
+p2Z = 0
+distance = math.sqrt((p2X - p1X) ** 2 + (p2Y - p1Y) ** 2 + (p2Z - p1Z) ** 2)
+print (f"6: the distance in 3D between p1 and p2 is {round(distance, 2)} units")
