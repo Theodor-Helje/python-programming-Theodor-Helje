6013ffc7ebd8d01c272c2acc0f042bd751730105
Theodor Helje <Theodor.Helje@iths.se>
2025-10-06 11:50:48 +0200

    Finished exercises 12.1

--- Exercises/Exercises12.py
+++ Exercises/Exercises12.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Video:
-    def __init__(self, title=None, genre=None, rating = .0, type=None):
+    """Class to describe a general video, used as a building block for other classes"""
+    def __init__(self, title=None, genre=None, rating = None):
         self.title = title
         self.genre = genre
         self.rating = rating
@@ -19,10 +20,6 @@ class Video:
     def rating(self):
         return self._rating
     
-    @property
-    def type(self):
-        return self._type
-    
     @title.setter
     def title(self, title):
         self._title = title
@@ -35,9 +32,51 @@ class Video:
     def rating(self, rating):
         self._rating = rating
 
-    @type.setter
-    def type(self, type):
-        self._type = type
-
     def info(self):
-        return f"{self.type} with title {self.title}, genre: {self.genre}, rating {self.rating}"
+        return f"video with title {self.title}, genre: {self.genre}, rating {self.rating} \n"
+    
+class TV_serie(Video):
+    """Class to describe a TV-series, uses the video class as parent"""
+    def __init__(self, title=None, genre=None, rating=None, num_episodes=None):
+        super().__init__(title, genre, rating)
+        self.num_episodes = num_episodes
+    
+    @property
+    def num_episodes(self):
+        return self._num_episodes
+    
+    @num_episodes.setter
+    def num_episodes(self, num_episodes):
+        self._num_episodes = num_episodes
+    
+    def info(self):
+        return f"TV-series, title: {self.title}, genre: {self.genre}, rating: {self.rating}, episodes: {self.num_episodes} \n"
+
+class Movie(Video):
+    """Class to describe a movie, uses the video class as parent"""
+    def __init__(self, title=None, genre=None, rating=None, duration=None):
+        super().__init__(title, genre, rating)
+        self.duration = duration
+    
+    @property
+    def duration(self):
+        return self._duration
+    
+    @duration.setter
+    def duration(self, duration):
+        self._duration = duration
+    
+    def info(self):
+        return f"Movie, title: {self.title}, genre: {self.genre}, rating: {self.rating}, duraition: {self.duration} \n"
+    
+class Documentary(Video):
+    """Class to describe a documentary, uses the video class as parent"""
+    def __init__(self, title=None, genre=None, rating=None):
+        super().__init__(title, genre, rating)
+
+pokemon = TV_serie("Pokemon", "Cartoon", 4.5, 550)
+titanic = Movie("Titanic", "Romance", 4.7, 194)
+code = Documentary("The Code", "Math", 4)
+
+for video in tuple((pokemon, titanic, code)):
+    print(video.info())
