a26d8569142821c1320c6c37d57ea23950bafacb
Theodor Helje <Theodor.Helje@iths.se>
2025-10-29 12:55:35 +0100

    Started work on exercises 00.03

--- Databehandling/Exercises/Exercises00.py
+++ Databehandling/Exercises/Exercises00.py
@@ -2,7 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+show_plots = False
 
+def read_komtopp50(sheet=0):
+    name_list = ["Rang 2020", "Rang 2019", "Kommun", "Folkmängd 2020", "Folkmängd 2019", "förändring"]
+    return pd.read_excel("Data/komtopp50_2020.xlsx", names=name_list, sheet_name=sheet, skiprows=6)
 #1
 cities_and_population = pd.DataFrame({
     'kommun': ['Malmö', 'Stockholm', 'Uppsala', 'Göteborg'],
@@ -19,18 +23,24 @@ print(f"E: Andel befolkning i %\n{cities_and_population}\n")
 
 #2
 print(f"Sheet names:\n{pd.ExcelFile("Data/komtopp50_2020.xlsx").sheet_names}\n")
-name_list = ["Rang 2020", "Rang 2019", "Kommun", "Folkmängd 2020", "Folkmängd 2019", "förändring"]
-cities_sweden = pd.read_excel("Data/komtopp50_2020.xlsx", names=name_list, sheet_name=1, skiprows=6).sort_values(by="Rang 2020")
+cities_sweden = read_komtopp50(1)
 print(f"2a, b: Cleaned data\n{cities_sweden.head()}\n")
 print(f"2c: Sorted after population\n{cities_sweden.sort_values(by="Rang 2020")}\n")
 print(f"2d: The five smallest cities\n{cities_sweden.sort_values(by="Rang 2020", ascending=False).head()}\n")
 print(f"2e: Swedens population 2019, 2020\n{cities_sweden["Folkmängd 2019"].sum()}, {cities_sweden["Folkmängd 2020"].sum()}\n")
 
-fig, axes = plt.subplots(1, 2, figsize=(11,5))
-sns.barplot(data=cities_sweden.head(5), x="Kommun", y="Folkmängd 2020", palette='pastel', ax=axes[0])
-axes[0].set_title("2f: Sveriges 5 största kommuner 2020")
-sns.barplot(data=cities_sweden.tail(5), x="Kommun", y="Folkmängd 2020", palette='pastel', ax=axes[1])
-axes[1].set_title("2f: Sveriges 5 minsta kommuner 2020")
-plt.show()
+if show_plots:
+    fig, axes = plt.subplots(1, 2, figsize=(11,5))
+    sns.barplot(data=cities_sweden.head(5), x="Kommun", y="Folkmängd 2020", palette='pastel', ax=axes[0])
+    axes[0].set_title("2f: Sveriges 5 största kommuner 2020")
+    sns.barplot(data=cities_sweden.tail(5), x="Kommun", y="Folkmängd 2020", palette='pastel', ax=axes[1])
+    axes[1].set_title("2f: Sveriges 5 minsta kommuner 2020")
+    plt.show()
 
 #3
+cities_sweden_female, cities_sweden_male = read_komtopp50(2), read_komtopp50(3) #byt förändring till förändring %
+cities_sweden_female["kön"], cities_sweden_male["Kön"] = "Kvinna", "Man"
+print(f"3a: female\n{cities_sweden_female.head()}\n")
+print(f"3a: male\n{cities_sweden_male.head()}\n")
+cities_gender_merged = pd.concat([cities_sweden_female, cities_sweden_male])
+print(cities_gender_merged)
