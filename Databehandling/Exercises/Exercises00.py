4796fc45d0bae659980a993419c6ec8b3f8a5fec
Theodor Helje <Theodor.Helje@iths.se>
2025-10-29 16:09:24 +0100

    Continued work on exercises 00.03

--- Databehandling/Exercises/Exercises00.py
+++ Databehandling/Exercises/Exercises00.py
@@ -2,7 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-show_plots = False
+show_plots = True
+#jag har väldigt medvetet valt att återanvända viss kod (som t.ex. barplots) för enkelheten
+#jag är smått missnöjd över hur lång rader jag har skrivit men det är för enkelt och tidskrävande att skriva allt mindre och i funktioner
+#också onöjd med att jag använder "" föt DF och inte ''
 
 def read_komtopp50(sheet=0):
     name_list = ["Rang 2020", "Rang 2019", "Kommun", "Folkmängd 2020", "Folkmängd 2019", "förändring"]
@@ -38,9 +41,39 @@ if show_plots:
     plt.show()
 
 #3
-cities_sweden_female, cities_sweden_male = read_komtopp50(2), read_komtopp50(3) #byt förändring till förändring %
-cities_sweden_female["kön"], cities_sweden_male["Kön"] = "Kvinna", "Man"
+cities_sweden_female, cities_sweden_male = read_komtopp50(2), read_komtopp50(3)
+cities_sweden_female["Kön"], cities_sweden_male["Kön"] = "Kvinna", "Man"
 print(f"3a: female\n{cities_sweden_female.head()}\n")
 print(f"3a: male\n{cities_sweden_male.head()}\n")
+
 cities_gender_merged = pd.concat([cities_sweden_female, cities_sweden_male])
-print(cities_gender_merged)
+del cities_gender_merged["Rang 2020"], cities_gender_merged["Rang 2019"]
+print(f"3b: merged male / female\n{cities_gender_merged}\n")
+
+del cities_sweden["Rang 2020"], cities_sweden["Rang 2019"]
+cities_sweden.columns = ["Kommun", "Total Pop 2020", "Total Pop 2019", "Total förändring"]
+print(f"3c: modified total\n{cities_sweden.head()}\n")
+
+cities_sweden_final = pd.merge(cities_gender_merged.set_index("Kommun"), cities_sweden.set_index("Kommun"), on="Kommun").sort_values(by="Total Pop 2020", ascending=False)
+print(f"3d: merged total and merged male / female\n{cities_sweden_final}\n")
+
+if show_plots:
+    fig, axes = plt.subplots(1, 2, figsize=(12,5))
+
+    sns.barplot(data=cities_sweden_final.sort_values("Total Pop 2020").head(10), x="Folkmängd 2020", y="Kommun", palette='pastel', hue="Kön", ax=axes[0])
+    axes[0].set_title("3e: Sveriges 10 största kommuner 2020")
+
+    sns.barplot(data=cities_sweden_final.sort_values("Total Pop 2020", ascending=True).tail(10), x="Folkmängd 2020", y="Kommun", palette='pastel', hue="Kön", ax=axes[1])
+    axes[1].set_title("3e: Sveriges 10 minsta kommuner 2020")
+
+    plt.show()
+
+    plt.pie([cities_sweden_final.loc[cities_sweden_final["Kön"] == "Man", "Folkmängd 2020"].sum(), cities_sweden_final.loc[cities_sweden_final["Kön"] == "Kvinna", "Folkmängd 2020"].sum()], 
+            labels=["Män", "Kvinnor"], 
+            startangle=90, 
+            autopct='%1.1f%%'
+            )
+    plt.title("3f: Könsfördelning i Sveriga 2020")
+    plt.show()
+
+    sns.barplot(data=pd.DataFrame({"gender difference": abs(cities_sweden_final.loc[cities_sweden_final["Kön"] == "Man", "Folkmängd 2020"] / cities_sweden_final.loc[cities_sweden_final["Kön"] == "Kvinna", "Folkmängd 2020"])})) #LIST COMPREHENSION
