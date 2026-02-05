60adf61af9533d0b3fa27cbff0abd652cd0f0099
Theodor Helje <Theodor.Helje@iths.se>
2026-02-05 11:35:00 +0100

    continued work on labb

--- Statistiska metoder/Labb/labb_class.py
+++ Statistiska metoder/Labb/labb_class.py
@@ -5,7 +5,7 @@ class LinearRegression:
     def __init__(self, X=None, Y=None):
         self._X = None
         self._Y = None
-        self._coef = None
+        self._b= None
 
         if X is not None and Y is not None:
             self.X = X
@@ -40,28 +40,53 @@ class LinearRegression:
             return self.X.shape[1]-1
         else:
             return None
+
+    @property
+    def SSE(self):
+        if self.X is not None and self.Y is not None and self.b is not None:
+            return np.sum(np.square(self.Y - (self.X @ self.b)))
+        else:
+            return None
     
     @property
-    def coef(self):
-        return self._coef
+    def MSE(self):
+        return np.divide(self.SSE, self.n)
+    
+    @property
+    def b(self):
+        if self._b is None:
+            self.fit()
+
+        return self._b
+    
+    @property
+    def var(self):
+        return np.divide(self.SSE, (self.n - self.d - 1))
+    
+    @property
+    def std(self):
+        return np.sqrt(self.SSE)
     
     def pred(self, X):
         _X = np.asarray(X)
-        return np.column_stack(np.ones(_X.shape[0]), _X) @ self.coef #FIX
-    
-    def variance(self):
-        pass
 
-    def std(self):
-        pass
+        if _X.ndim == 1:
+            _X = _X.reshape(1, -1)
+
+        return (np.column_stack((np.ones(_X.shape[0]), _X)) @ self.b).flatten()
 
     def fit(self, X=None, Y=None):
         if X is not None:
             self.X = X
+        elif self.X is None:
+            raise ValueError("You must define X and Y before comuting bias")
+        
         if Y is not None:
             self.Y = Y
+        elif self.Y is None:
+            raise ValueError("You must define X and Y before comuting bias")
         
-        self._coef = np.linalg.pinv(self.X.T @ self.X) @ self.X.T @ self.Y
+        self._b = np.linalg.pinv(self.X.T @ self.X) @ self.X.T @ self.Y
 
         return self
 
@@ -70,9 +95,14 @@ class LinearRegression:
 
 
 if __name__ == "__main__":
-    lin_reg = LinearRegression(X=[1, 2, 3, 4], Y=[10, 15, 20, 25])
+    lin_reg = LinearRegression(X=[[1, 2, 3, 4], [2, 7, 3, 8], [1, 8, 4, 6], [2,5 ,3 ,7]], Y=[10, 15, 20, 25])
     print(f"X:\n{lin_reg.X}\n")
     print(f"Y:\n{lin_reg.Y}\n")
-    print(f"coef:\n{lin_reg.coef}\n")
+    print(f"coef:\n{lin_reg.b}\n")
     print(f"d: {lin_reg.d}")
     print(f"n: {lin_reg.n}")
+    print(f"SSE: {lin_reg.SSE}")
+    print(f"MSE: {lin_reg.MSE}")
+    print(f"var: {lin_reg.var}")
+    print(f"std: {lin_reg.std}")
+    print(lin_reg.pred([2, 5, 7, 3]))
