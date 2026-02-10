d09a8cc2d823de62a960e455e61206c1b5d6af2e
Theodor Helje <Theodor.Helje@iths.se>
2026-02-10 11:08:01 +0100

    added corr() method

---
+++ Statistiska metoder/Labb/linear_regression_class.py
@@ -0,0 +1,155 @@
+import numpy as np
+import scipy.stats as stats
+
+class LinearRegression:
+    def __init__(self, X=None, Y=None, confidence_level=95):
+        self._X = None
+        self._Y = None
+        self._b= None
+        self.confidence_level = confidence_level #add and implement completely
+
+        if X is not None and Y is not None:
+            self.X = X
+            self.Y = Y
+            self.fit()
+    
+    @property
+    def X(self):
+        return self._X
+    @X.setter
+    def X(self, X):
+        _X = np.asarray(X)
+        self._X = np.column_stack([np.ones(_X.shape[0]), _X])
+    
+    @property
+    def Y(self):
+        return self._Y
+    @Y.setter
+    def Y(self, Y):
+        self._Y = np.asarray(Y).reshape(-1, 1)
+    
+    @property
+    def confidence_level(self):
+        return self._confidence_level
+    @confidence_level.setter
+    def confidence_level(self, conf_level):
+        self._confidence_level = conf_level
+
+    @property
+    def n(self):
+        if self.X is not None:
+            return self.X.shape[0]
+        else:
+            return None
+
+    @property
+    def d(self):
+        if self.X is not None:
+            return self.X.shape[1]-1
+        else:
+            return None
+
+    @property
+    def SSE(self):
+        if self.X is not None and self.Y is not None and self.b is not None:
+            return np.sum(np.square(self.Y - (self.X @ self.b)))
+        else:
+            raise ValueError("You must define X and Y before computing SSE")
+    
+    @property
+    def MSE(self):
+        return np.divide(self.SSE, self.n)
+    
+    @property
+    def RMSE(self):
+        return np.sqrt(self.MSE)
+    
+    @property
+    def Syy(self):
+        if self.Y is not None:
+            return np.sum(np.square(self.Y - np.mean(self.Y)))
+        else:
+            raise ValueError("You must define Y before computing Syy")
+    
+    @property
+    def SSR(self):
+        return self.Syy - self.SSE
+    
+    @property
+    def Rsq(self):
+        return np.divide(self.SSR, self.Syy)
+    
+    @property
+    def MSR(self):
+        return np.divide(self.SSR, self.d)
+    
+    @property
+    def b(self):
+        if self._b is None:
+            self.fit()
+
+        return self._b
+    
+    @property
+    def var(self):
+        return np.divide(self.SSE, (self.n - self.d - 1))
+    
+    @property
+    def std(self):
+        return np.sqrt(self.SSE)
+    
+    def corr(self):
+        _corr_matrix = np.empty((self.d, self.d))
+        for i in range(self.d):
+            for j in range(self.d):
+                _corr_matrix[i, j] = stats.pearsonr(self.X[:, i + 1], self.X[:, j + 1])[0]
+        return _corr_matrix
+    
+    def pred(self, X):
+        _X = np.asarray(X)
+
+        if _X.ndim == 1:
+            _X = _X.reshape(1, -1)
+
+        return (np.column_stack((np.ones(_X.shape[0]), _X)) @ self.b).flatten()
+
+    def fit(self, X=None, Y=None):
+        if X is not None:
+            self.X = X
+        elif self.X is None:
+            raise ValueError("You must define X and Y before comupting bias")
+        
+        if Y is not None:
+            self.Y = Y
+        elif self.Y is None:
+            raise ValueError("You must define X and Y before computing bias")
+        
+        self._b = np.linalg.pinv(self.X.T @ self.X) @ self.X.T @ self.Y
+
+        return self
+
+    def __repr__(self):
+        return f"LinearRegression(n={self.n}, d={self.d})"
+
+
+if __name__ == "__main__":
+    lin_reg = LinearRegression(
+        X=[[1, 2, 3], [2, 7, 3], [1, 8, 4], [2, 5 ,3], [1, 6, 7]], 
+        Y=[10, 15, 20, 25, 35]
+    )
+    print(f"X:\n{lin_reg.X}\n")
+    print(f"Y:\n{lin_reg.Y}\n")
+    print(f"coef:\n{lin_reg.b}\n")
+    print(f"d: {lin_reg.d}")
+    print(f"n: {lin_reg.n}")
+    print(f"SSE: {lin_reg.SSE}")
+    print(f"MSE: {lin_reg.MSE}")
+    print(f"RMSE:: {lin_reg.RMSE}")
+    print(f"Syy: {lin_reg.Syy}")
+    print(f"SSR: {lin_reg.SSR}")
+    print(f"Rsq: {lin_reg.Rsq}")
+    print(f"MSR: {lin_reg.MSR}")
+    print(f"var: {lin_reg.var}")
+    print(f"std: {lin_reg.std}")
+    print(f"pred test: {lin_reg.pred([2, 5, 7])}")
+    print(f"corr:\n{lin_reg.corr()}")
